20e8d9064935559a8f2f08ab6e977327f340b37c
OGTutor <i.send.you.a.letter@gmail.com>
2023-07-27 00:37:35 +0300

    Some fixes & added some styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Header from '@/components/Header';
+
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			Quick Rental!
-		</main>
+		<header>
+			<Header />
+		</header>
 	);
 }
