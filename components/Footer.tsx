f680b4a5b155cb486596460ed77c8cd49bb27d7f
OGTutor <i.send.you.a.letter@gmail.com>
2023-08-24 18:56:19 +0300

    Implemented a basic Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+const Footer: FC = () => {
+	return (
+		<div
+			className="container mx-auto px-4 flex
+			flex-col items-center justify-center md:justify-between"
+		>
+			FooterContent
+		</div>
+	);
+};
+
+export default Footer;
