7ff11eb8eea167f04ae1a7c74f92d9fe994446b3
OGTutor <i.send.you.a.letter@gmail.com>
2023-08-24 18:54:30 +0300

    Added Header, Main & Footer sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import Main from '@/components/Main';
 
 export default function Home() {
 	return (
-		<header>
-			<Header />
-		</header>
+		<>
+			<header>
+				<Header />
+			</header>
+			<main>
+				<Main />
+			</main>
+			<footer>
+				<Footer />
+			</footer>
+		</>
 	);
 }
