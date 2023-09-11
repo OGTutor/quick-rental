8c3a3dff4c043926261d4c0c92202542891d0fea
OGTutor <i.send.you.a.letter@gmail.com>
2023-09-11 19:56:29 +0300

    Improved Input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -10,12 +10,12 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
 			<input
 				type={type}
 				className={twMerge(
-					`flex w-full rounded-full bg-slate-50
-					border border-gray-400 px-3 py-3 text-sm
+					`flex w-full rounded-md bg-slate-50
+					border border-l-main border-r-main px-3 py-3 text-sm
 					file:border-0 file:bg-transparent file:text-sm
 					file:font-medium placeholder:text-neutral-400
 					disabled:cursor-not-allowed disabled:opacity-50
-					focus:outline-none`,
+					focus:outline-none focus:border-main transition`,
 					disabled && 'opacity-75',
 					className
 				)}
