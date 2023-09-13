6b362d4524f466272a48a11d133f7fafb62a456b
OGTutor <i.send.you.a.letter@gmail.com>
2023-09-13 19:48:54 +0300

    Implemented about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,21 @@
+import Footer from '@/components/Footer';
+import GeneralHeader from '@/components/GeneralHeader';
+import AboutContent from './components/AboutContent';
+
 const About = () => {
-	return <div>About page</div>;
+	return (
+		<>
+			<header>
+				<GeneralHeader title="About" subTitle="About" />
+			</header>
+			<main>
+				<AboutContent />
+			</main>
+			<footer>
+				<Footer />
+			</footer>
+		</>
+	);
 };
 
 export default About;
